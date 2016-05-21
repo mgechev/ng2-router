c187fa26ce6aa95207b22944865039e9326e9ef3
mgechev <mgechev@gmail.com>
2016-05-21 14:42:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Fork of `router-deprecated` with a bonus.
+
+# License
+
+MIT
+
---
+++ typings.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="./typings/main.d.ts" />
+/// <reference path="./node_modules/zone.js/dist/zone.js.d.ts" />
