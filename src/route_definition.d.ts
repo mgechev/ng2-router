1b46989526cdbd2328abaa16ea1f25ca6820cb30
mgechev <mgechev@gmail.com>
2016-05-21 18:09:04 +0300

    Initial commit

---
+++ src/route_definition.d.ts
@@ -0,0 +1,48 @@
+import { Type } from '../src/facade/lang';
+import { RegexSerializer } from './rules/route_paths/regex_route_path';
+/**
+ * `RouteDefinition` defines a route within a {@link RouteConfig} decorator.
+ *
+ * Supported keys:
+ * - `path` or `aux` (requires exactly one of these)
+ * - `component`, `loader`,  `redirectTo` (requires exactly one of these)
+ * - `name` or `as` (optional) (requires exactly one of these)
+ * - `data` (optional)
+ *
+ * See also {@link Route}, {@link AsyncRoute}, {@link AuxRoute}, and {@link Redirect}.
+ */
+export interface RouteDefinition {
+    path?: string;
+    aux?: string;
+    regex?: string;
+    serializer?: RegexSerializer;
+    component?: Type | ComponentDefinition;
+    loader?: () => Promise<Type>;
+    redirectTo?: any[];
+    as?: string;
+    name?: string;
+    data?: any;
+    useAsDefault?: boolean;
+    defer?: Defer;
+}
+/**
+ * Represents either a component type (`type` is `component`) or a loader function
+ * (`type` is `loader`).
+ *
+ * See also {@link RouteDefinition}.
+ */
+export interface ComponentDefinition {
+    type: string;
+    loader?: () => Promise<Type>;
+    component?: Type;
+}
+/**
+ * An object which needs to be resolved until we are able to render the route component.
+ *
+ * @example
+ * const baz: Defer = { resolve: () => Promise.resolve(), deps: [] };
+ */
+export interface Defer {
+    resolve: (...deps: any[]) => Promise<any>;
+    deps?: any[];
+}
